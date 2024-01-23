c96c8a040b1755f10c9d8267cef157dbc9e34276
Belle Franzen <isabellefranzen.job@gmail.com>
2024-01-23 10:57:26 -0300

    exercicio08

--- exercicio08/exercicio08.cs
+++ exercicio08/exercicio08.cs
@@ -1,1 +1,27 @@
-﻿
+﻿/* Código simulando uma jogada de moeda.
+
+Random moeda = new Random();
+int jogada = moeda.Next(0,2);
+
+Console.WriteLine((jogada == 0) ? "CARA" : "COROA");*/
+
+
+// Avaliar quem pode receber acesso a algum servidor baseado no seu título e nível.
+string permission = "Admin|Gerente";
+int level = 16;
+
+if (permission.Contains("Admin") && level > 55){
+    Console.WriteLine("Bem-vindo Super Administrador.");
+
+} else if (permission.Contains("Admin") && level <= 55){
+    Console.WriteLine("Bem-vindo Administrador.");
+
+} else if (permission.Contains("Gerente") && level >= 20) {
+    Console.WriteLine("Contate o Administrador para acessar.");
+
+} else if (permission.Contains("Gerente") && level < 20) {
+    Console.WriteLine("Você não possui privilégios nesse acesso.");
+
+} else {
+    Console.WriteLine("Você não pode ter acesso.");
+}
