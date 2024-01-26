0992f284dc48125ebf2f532558d47cfea33aa305
Belle Franzen <isabellefranzen.job@gmail.com>
2024-01-26 11:01:26 -0300

    exercicios
    Exercícios realizados

---
+++ exercicio09/exercicio09.cs
@@ -0,0 +1,59 @@
+﻿string cdp = "01-MN-G";
+
+string[] produto = cdp.Split('-');
+
+string tipo = "";
+string cor = "";
+string tamanho = "";
+
+switch(produto[0]){
+    case "01":
+    tipo = "Blusão";
+    break;
+
+    case "02":
+    tipo = "Camiseta";
+    break;
+
+    case "03":
+    tipo = "Calça";
+    break;
+
+    default:
+    tipo = "Outros";
+    break;
+
+}
+switch(produto[1]){
+     case "PT":
+    cor = "Preto";
+    break;
+
+    case "MN":
+    cor = "Marrom";
+    break;
+
+    default:
+    cor = "Branco";
+    break;
+}
+
+switch(produto[2]){
+     case "P":
+    tamanho = "Pequeno";
+    break;
+
+    case "M":
+    tamanho = "Medio";
+    break;
+
+    case "G":
+    tamanho = "Grande";
+    break;
+
+    default:
+    tamanho = "Tamanho Universal";
+    break;
+}
+
+Console.WriteLine($"Produto: {tipo} {cor} {tamanho}");
