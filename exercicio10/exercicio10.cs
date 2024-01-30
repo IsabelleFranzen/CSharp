852b4fbdb6bd3ffab4759cf72fde0cce4a50c051
Belle Franzen <isabellefranzen.job@gmail.com>
2024-01-30 11:48:01 -0300

    exercicios

---
+++ exercicio10/exercicio10.cs
@@ -0,0 +1,36 @@
+﻿/* Criar um jogo de RPG:
+O herói e o monstro começarão com dez pontos de integridade.
+Todos os ataques serão um valor entre 1 e 10.
+O herói atacará primeiro.
+Imprima a quantidade de integridade que o monstro perdeu e a integridade que resta a ele.
+Se a integridade do monstro for maior que zero, ele poderá atacar o herói.
+Imprima a quantidade de integridade que o herói perdeu e a integridade que resta a ele.
+Continue esta sequência de ataque até que a integridade do monstro ou do herói seja zero ou menos.
+Imprima quem foi o vencedor. 
+*/
+
+Random dados = new Random();
+int heroi = 10;
+int monstro = 10;
+
+do {
+    int dano = dados.Next(1,11);
+    monstro = monstro - dano;
+    Console.WriteLine($"O monstro tomou {dano} de dano. Restam {monstro} de HP.");
+
+    if (monstro <= 0) continue;
+
+        dano = dados.Next(1,11);
+        heroi = heroi - dano;
+        Console.WriteLine($"O herói tomou {dano} de dano. Restam {heroi} de HP.");
+    
+
+} while((heroi > 0) && (monstro > 0));
+
+if (heroi > monstro){
+    Console.WriteLine($"O herói venceu o turno.");
+} else {
+    Console.WriteLine($"O monstro venceu o turno.");
+}
+
+//Console.WriteLine(heroi > monstro ? "O herói venceu o turno!" : "O monstro venceu o turno!"); -- outra solução.
---
+++ exercicio11/exercicio11.cs
@@ -0,0 +1,28 @@
+﻿string? informacao;
+string valorInformado = "";
+int numero = 0;
+bool numeroValido = false;
+
+Console.WriteLine("Informe um número de 5 a 10.");
+
+do {
+    informacao = Console.ReadLine();
+    if (informacao != null ){
+        valorInformado = informacao;
+    }
+
+    numeroValido = int.TryParse(valorInformado, out numero);
+
+    if (numeroValido == true) {
+
+        if ((numero <= 5) || (numero >= 10)){
+        numeroValido =  false;
+        Console.WriteLine($"Você informou o número {numero}. Insira um número entre 5 e 10.");
+        }
+    } else {
+        Console.WriteLine("Você inseriu um número inválido, insira novamente.");
+    }
+
+} while (numeroValido == false);
+Console.WriteLine($"Você informou corretamente o número {numero}.");
+informacao = Console.ReadLine();
